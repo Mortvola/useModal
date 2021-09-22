de66327a3fcbd2abacb9af560a8b5603cf1aff29
Mortvola <mortvola@gmail.com>
2021-09-22 07:19:19 -0600

    Added bootstrap's Modal component.

--- src/useModal.tsx
+++ src/useModal.tsx
@@ -1,26 +1,29 @@
 import React, { useState, ReactElement, useCallback } from 'react';
+import { Modal } from 'react-bootstrap';
 
 type ShowCallback = (() => void);
 type OnSave = (() => void);
 type OnHide = (() => void);
 type OnConfirm = (() => void);
 
-type UseModalType<T> = [
+export type UseModalType<T> = [
   (props: T) => (ReactElement | null),
   ShowCallback,
 ];
 
+type SizeType = 'sm' | 'lg' | 'xl';
+
 export interface ModalProps {
-  show: boolean,
   setShow: (show: boolean) => void;
-  onHide: OnHide,
+  onHide?: OnHide,
   onConfirm?: OnConfirm,
 }
 
 function useModal<T>(
   Dialog: React.FC<T & ModalProps>,
+  size?: SizeType,
   onSave?: OnSave,
-): UseModalType<T> {
+): UseModalType<T & { onHide?: () => void }> {
   const [showDialog, setShowDialog] = useState(false);
 
   const createDialog = useCallback((props: T & { onHide?: () => void }): ReactElement | null => {
@@ -47,24 +50,19 @@ function useModal<T>(
       setShowDialog(false);
     };
 
-    if (showDialog) {
-      const props2 = {
-        ...props,
-        show: showDialog,
-        setShow: localSetShow,
-        onHide: handleHide,
-        onConfirm: handleSave,
-      };
-
-      return (
-        <Dialog
-          {...props2}
-        />
-      );
-    }
-
-    return null;
-  }, [Dialog, onSave, showDialog]);
+    return (
+      <Modal
+        show={showDialog}
+        onHide={handleHide}
+        size={size}
+        scrollable
+        enforceFocus={false}
+        contentClassName="modal-content-fix"
+      >
+        <Dialog {...props} setShow={localSetShow} onConfirm={handleSave} />
+      </Modal>
+    );
+  }, [Dialog, onSave, showDialog, size]);
 
   return [
     createDialog,
@@ -72,5 +70,9 @@ function useModal<T>(
   ];
 }
 
+export function makeUseModal<T>(Dialog: React.FC<T & ModalProps>, size?: SizeType) {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  return (): UseModalType<T & { onHide?: () => void }> => useModal<T>(Dialog, size);
+}
+
 export default useModal;
-export { UseModalType };
